9c3486101a96810c9832e6e0ee8bb55ef2ed78dd
sagar6462 <sagaarpkr@gmail.com>
2020-01-14 14:21:37 +0545

    First and final commit

---
+++ src/Square.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+function Square(props) {
+  return (
+    <div>
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    </div>
+  );
+}
+
+export default Square;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import * as serviceWorker from "./serviceWorker";
+
+import Game from "./Game";
+
+ReactDOM.render(<Game />, document.getElementById("root"));
